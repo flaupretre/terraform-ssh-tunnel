3fc3f184628cf3f67fc4275f73f184bc86b985a9
Ben Howes <ben@ben-howes.co.uk>
2023-09-27 10:32:34 +0100

    Apply fix from #25

--- gateways/ssm.sh
+++ gateways/ssm.sh
@@ -9,7 +9,7 @@ gw="$TUNNEL_GATEWAY_HOST"
 [ "X$TUNNEL_GATEWAY_USER" = X ] || gw="$TUNNEL_GATEWAY_USER@$TUNNEL_GATEWAY_HOST"
 
 $TUNNEL_SSH_CMD \
-  -o ProxyCommand "aws ssm start-session $TUNNEL_SSM_OPTIONS --target %h --document-name $TUNNEL_SSM_DOCUMENT_NAME --parameters 'portNumber=%p'" \
+  -o ProxyCommand="aws ssm start-session $TUNNEL_SSM_OPTIONS --target %h --document-name $TUNNEL_SSM_DOCUMENT_NAME --parameters 'portNumber=%p'" \
   -N \
   -L "$TUNNEL_LOCAL_HOST:$TUNNEL_LOCAL_PORT:$TUNNEL_TARGET_HOST:$TUNNEL_TARGET_PORT" \
   -p "$TUNNEL_GATEWAY_PORT" \
