dfc3a672b377cd79e0e167917c27b328faa904a3
Boris Smidt <boris.smidt@klarrio.com>
2024-02-12 11:49:47 +0100

    add support in ssm for AWS-StartPortForwardingSessionToRemoteHost

--- gateways/ssm.sh
+++ gateways/ssm.sh
@@ -15,12 +15,17 @@ if [ -n "$AWS_ASSUME_ROLE" ] ; then
   eval "$(aws sts assume-role --role-arn "$AWS_ASSUME_ROLE" --role-session-name="terraform-ssh-tunnel" --query 'Credentials.[AccessKeyId,SecretAccessKey,SessionToken]' --output text | awk '{ print "export AWS_ACCESS_KEY_ID=" $1 "\nexport AWS_SECRET_ACCESS_KEY=" $2 "\nexport AWS_SESSION_TOKEN=" $3 }')"
 fi
 
+if [ "$TUNNEL_SSM_DOCUMENT_NAME" == "AWS-StartPortForwardingSessionToRemoteHost" ]; then
+aws ssm start-session --target $TUNNEL_GATEWAY_HOST \
+      --document-name AWS-StartPortForwardingSessionToRemoteHost \
+      --parameters "{\"portNumber\":[\"$TUNNEL_TARGET_PORT\"],\"localPortNumber\":[\"$TUNNEL_LOCAL_PORT\"], \"host\":[\"$TUNNEL_TARGET_HOST\"]}" &
 
+elif ["$TUNNEL_SSM_DOCUMENT_NAME" == "AWS-StartSSHSession"]; then
 $TUNNEL_SSH_CMD \
   -o ProxyCommand="aws ssm start-session $TUNNEL_SSM_OPTIONS --target %h --document-name $TUNNEL_SSM_DOCUMENT_NAME --parameters 'portNumber=%p'" \
   -N \
   -L "$TUNNEL_LOCAL_HOST:$TUNNEL_LOCAL_PORT:$TUNNEL_TARGET_HOST:$TUNNEL_TARGET_PORT" \
   -p "$TUNNEL_GATEWAY_PORT" \
    "$gw" &
-
+fi
 TUNNEL_PID=$!
